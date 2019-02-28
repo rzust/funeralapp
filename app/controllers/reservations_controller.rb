a3c0266f2f5b22d576caf6fd3ac631ca01be198c
Renato <renatozust@mail.ru>
2019-02-28 16:56:42 +0100

    show available days and slots on days of calendar

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,25 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_customer!
+  before_action :set_reservations, only: [:index]
   respond_to :html
 
   def new
     @selected_day = DateTime.parse(params[:selected_day])
     reservations = Reservation.where(starting_time: @selected_day.beginning_of_day..@selected_day.end_of_day)
-    @reservations = reservations.map{|x| x.starting_time.hour}
+    @taken_slots = reservations.map{|x| x.starting_time.hour}
   end
 
   def create
     reservation    = Reservation.new(reservation_params)
     reservation.end_time = reservation.starting_time + 2.hours
     flash[:notice] = "Reservation successfully created!" if reservation.save
-    render :index
+    redirect_to action: "index"
+  end
+
+private
+  def set_reservations
+    start_date = params[:start_date].present? ? DateTime.parse(params[:start_date]) : DateTime.now
+    @reservations = Reservation.where(starting_time: start_date.beginning_of_day..(start_date+34.days).end_of_day)
   end
 
   def reservation_params
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,8 @@ class Reservation < ApplicationRecord
 
   validates :starting_time, presence: true, uniqueness: true
 
+  def start_time
+    self.starting_time
+  end
+
 end
