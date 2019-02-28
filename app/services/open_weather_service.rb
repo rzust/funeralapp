a7ec3be6b10dcd0ec69e32ab8fd5b7055866f48b
Renato <renatozust@mail.ru>
2019-02-28 13:13:34 +0100

    use dig in credential loading

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,7 +1,7 @@
 class OpenWeatherService
 
   def get_forecast
-    api_key = Rails.application.credentials[:open_weather][:key]
+    api_key = Rails.application.credentials.dig(:open_weather, :key)
     @client  = Faraday.new
     response = @client.get("http://api.openweathermap.org/data/2.5/forecast?id=3196359&units=metric&APPID=#{api_key}")
   end
