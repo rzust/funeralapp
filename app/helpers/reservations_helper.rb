25a05289202995a201f4fd87eb926a3a6f540eb2
Renato <renatozust@mail.ru>
2019-03-02 16:03:26 +0100

    show forecast in reservation views

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,13 @@
 module ReservationsHelper
+  def five_day_forecast
+    response = OpenWeatherService.new.get_forecast
+    forecast = {}
+    response.dig("list").each do |x|
+      key  = x['dt'] - x['dt'] % 86400
+      date = DateTime.strptime(key.to_s, "%s")
+      forecast[key] = [] unless forecast[key].present?
+      forecast[key].push({date: date, weather: x.dig("weather", 0, "main"), temp: x.dig('main','temp')})
+    end
+    forecast.each{ |key, val| forecast[key] = val[val.count/2] }
+  end
 end
