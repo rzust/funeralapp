d10b7e45e85e10ee6118e6596d92396021763f54
Renato <renatozust@mail.ru>
2019-03-02 15:56:46 +0100

    erase blank lines

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -10,16 +10,11 @@ class OpenWeatherService
   end
 
   def get_forecast
-    api_key = Rails.application.credentials.dig(:open_weather, :key)
-
-    started = Time.now
-
+    api_key  = Rails.application.credentials.dig(:open_weather, :key)
+    started  = Time.now
     response = @client.get "http://api.openweathermap.org/data/2.5/forecast?id=3196359&units=metric&APPID=#{api_key}"
-
     finished = Time.now
-
     puts "Get forecast request took #{(finished - started) * 1000} ms."
-
     response.body
   end
 
